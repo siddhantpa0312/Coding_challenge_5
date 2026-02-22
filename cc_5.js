e0dffbdc23f015adf588365a4b3ec0a00097c65d
Siddhant Pandey <siddhant.pa2002@gmail.com>
2026-02-22 13:29:58 -0500

    Created arrays and checked output

---
+++ cc_5.js
@@ -0,0 +1,25 @@
+const employees = [
+    {
+        name:"Brent",
+        hourlyRate: 29,
+        hoursWorked: 87
+    },
+    {
+        name:"Aron",
+        hourlyRate: 23,
+        hoursWorked: 65
+    },
+    {
+        name:"Chris",
+        hourlyRate: 18,
+        hoursWorked: 70
+    },
+    {
+        name:"Eve",
+        hourlyRate: 26,
+        hoursWorked: 75
+    }
+];
+
+//Test console.log(employees);
+
